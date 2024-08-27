55f02f5de22d9f9c63289bad6082623d9478d54e
renaebisawa <rean.main0624@gmail.com>
2024-08-27 11:03:50 +0900

    first commit

---
+++ practice/app.js
@@ -0,0 +1,27 @@
+let text = 'JavaScriptの練習';
+console.log(text);
+
+text = 'JavaScriptをマスターした';
+console.log(text);
+
+// 処理①
+let longText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+console.log(longText);
+console.log(longText);
+console.log(longText);
+
+// 処理②
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+
+let name = 'ジョニー';
+let greet = '私は' + name + '！';
+console.log(greet);
